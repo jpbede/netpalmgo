1d5e9b8fea7aeefefb5b248c19bd5a668c517e89
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-17 20:13:43 +0100

    Some tests

---
+++ apis/getconfig/client_test.go
@@ -0,0 +1,14 @@
+package getconfig
+
+import (
+	"github.com/go-resty/resty/v2"
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	cl := New(resty.New())
+
+	if cl == nil {
+		t.Error("Got no client")
+	}
+}
