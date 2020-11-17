00a95672c78a55e876218003b8ddc550d3c2d461
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-17 19:52:55 +0100

    Make codacy happy

--- client.go
+++ client.go
@@ -9,42 +9,42 @@ import (
 
 // Client is the main client of netpalmgo
 type Client struct {
-	resty *resty.Client
+	transport *resty.Client
 
 	getConfig getconfig.Client
 	task      task.Client
 }
 
-func newResty(apiUrl, apiKey string, httpClient *http.Client) *resty.Client {
+func newTransport(apiURL, apiKey string, httpClient *http.Client) *resty.Client {
 	var r *resty.Client
 	if httpClient != nil {
 		r = resty.NewWithClient(httpClient)
 	} else {
 		r = resty.New()
 	}
-	r.SetHostURL(apiUrl)
+	r.SetHostURL(apiURL)
 	r.SetHeader("x-api-key", apiKey)
 	return r
 }
 
 // New creates a new Client with APIUrl and APIKey
-func New(apiUrl, apiKey string) *Client {
+func New(apiURL, apiKey string) *Client {
 	c := Client{}
-	c.resty = newResty(apiUrl, apiKey, nil)
+	c.transport = newTransport(apiURL, apiKey, nil)
 	return &c
 }
 
 // NewWithClient creates a new Client with APIUrl and APIKey with a given http.Client
-func NewWithClient(apiUrl, apiKey string, httpClient *http.Client) *Client {
+func NewWithClient(apiURL, apiKey string, httpClient *http.Client) *Client {
 	c := Client{}
-	c.resty = newResty(apiUrl, apiKey, httpClient)
+	c.transport = newTransport(apiURL, apiKey, httpClient)
 	return &c
 }
 
 // GetConfig returns the client for the Endpoint /getconfig
 func (c *Client) GetConfig() getconfig.Client {
 	if c.getConfig == nil {
-		c.getConfig = getconfig.New(c.resty)
+		c.getConfig = getconfig.New(c.transport)
 	}
 	return c.getConfig
 }
@@ -52,7 +52,7 @@ func (c *Client) GetConfig() getconfig.Client {
 // Task returns the client for the Endpoint /task
 func (c *Client) Task() task.Client {
 	if c.task == nil {
-		c.task = task.New(c.resty)
+		c.task = task.New(c.transport)
 	}
 	return c.task
 }
