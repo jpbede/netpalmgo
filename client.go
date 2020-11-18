c3323eeec90e9126b0fa45a8b78f2629113dcc28
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-18 15:42:39 +0100

    Some cleanup

--- client.go
+++ client.go
@@ -15,36 +15,29 @@ type Client struct {
 	task      task.Client
 }
 
-func newTransport(apiURL, apiKey string, httpClient *http.Client) *transport.Client {
-	return transport.NewClient(apiURL, apiKey, httpClient)
-}
-
 // New creates a new Client with APIUrl and APIKey
 func New(apiURL, apiKey string) *Client {
-	c := Client{}
-	c.transport = newTransport(apiURL, apiKey, nil)
-	return &c
+	return NewWithClient(apiURL, apiKey, nil)
 }
 
 // NewWithClient creates a new Client with APIUrl and APIKey with a given http.Client
 func NewWithClient(apiURL, apiKey string, httpClient *http.Client) *Client {
-	c := Client{}
-	c.transport = newTransport(apiURL, apiKey, httpClient)
+	c := Client{
+		transport: transport.NewClient(apiURL, apiKey, httpClient),
+	}
+
+	c.getConfig = getconfig.New(c.transport)
+	c.task = task.New(c.transport)
+
 	return &c
 }
 
 // GetConfig returns the client for the Endpoint /getconfig
 func (c *Client) GetConfig() getconfig.Client {
-	if c.getConfig == nil {
-		c.getConfig = getconfig.New(c.transport)
-	}
 	return c.getConfig
 }
 
 // Task returns the client for the Endpoint /task
 func (c *Client) Task() task.Client {
-	if c.task == nil {
-		c.task = task.New(c.transport)
-	}
 	return c.task
 }
