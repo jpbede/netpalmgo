2e514d4a6b7c49627ec1e25c13d47551a5c575a5
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-18 13:06:21 +0100

    Some more test for /task

--- apis/task/func_get_test.go
+++ apis/task/func_get_test.go
@@ -8,30 +8,47 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
+
+func getResponse(taskID string, taskStatus models.TaskStatus) models.Response {
+	return models.Response{
+		Status: models.StatusSuccess,
+		Data: models.TaskResponse{
+			TaskID:     taskID,
+			TaskStatus: taskStatus,
+		},
+	}
+}
 
 func TestClient_GetWithTaskResponse(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		// test successful json response
 		if req.URL.String() == "/task/123" {
 			// Send response to be tested
-			resp, err := json.Marshal(models.Response{
-				Status: models.StatusSuccess,
-				Data: models.TaskResponse{
-					TaskID:     "123",
-					TaskStatus: models.TaskStatusFinished,
-				},
-			})
+			resp, err := json.Marshal(getResponse("123", models.TaskStatusFinished))
 			assert.NoError(t, err)
 			rw.Write(resp)
 		}
 
+		// test invalid json response
 		if req.URL.String() == "/task/1234" {
 			rw.Write([]byte("no json"))
 		}
+
+		// test token response
+		if req.URL.String() == "/task/12345" {
+			http.Error(rw, "{\"detail\":\"Could not validate credentials\"}", http.StatusForbidden)
+		}
+
+		// test client error
+		if req.URL.String() == "/task/123456" {
+			time.Sleep(2 * time.Second)
+			http.Error(rw, "{\"detail\":\"Could not validate credentials\"}", http.StatusForbidden)
+		}
 	}))
 
-	taskCl := New(resty.NewWithClient(server.Client()).SetHostURL(server.URL))
+	taskCl := New(resty.NewWithClient(server.Client()).SetHostURL(server.URL).SetTimeout(1 * time.Second))
 
 	// test successful response
 	getConfigResp := models.TaskResponse{
@@ -49,4 +66,20 @@ func TestClient_GetWithTaskResponse(t *testing.T) {
 	resp, err = taskCl.GetWithTaskResponse(getConfigResp)
 	assert.Error(t, err)
 	assert.EqualError(t, err, "invalid character 'o' in literal null (expecting 'u')")
+
+	// test invalid token response
+	getConfigResp = models.TaskResponse{
+		TaskID: "12345",
+	}
+	resp, err = taskCl.GetWithTaskResponse(getConfigResp)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "Could not validate credentials")
+
+	// test client error
+	getConfigResp = models.TaskResponse{
+		TaskID: "123456",
+	}
+	resp, err = taskCl.GetWithTaskResponse(getConfigResp)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "context deadline exceeded (Client.Timeout exceeded while awaiting headers)")
 }
