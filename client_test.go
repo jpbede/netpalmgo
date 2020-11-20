7dc827a64a4a7baf1048b53f4ce98e5b152d5bf3
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-20 12:25:18 +0100

    Testing: Test for WaitForResult

--- client_test.go
+++ client_test.go
@@ -1,7 +1,12 @@
 package netpalmgo
 
 import (
+	"context"
+	testing2 "github.com/jpbede/netpalmgo/internal/testing"
+	"github.com/jpbede/netpalmgo/models"
+	"github.com/stretchr/testify/assert"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -30,6 +35,29 @@ func TestNewWithClient(t *testing.T) {
 	if cl.transport == nil {
 		t.Error("Resty client is empty")
 	}
+}
+
+func TestWaitForResult(t *testing.T) {
+	reqcounter := 0
+
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		// Send response to be tested
+		if reqcounter > 2 {
+			rw.Write(testing2.GetTaskResponseJSON("123", models.TaskStatusFinished))
+		} else {
+			rw.Write(testing2.GetTaskResponseJSON("123", models.TaskStatusQueued))
+		}
+
+		reqcounter++
+	}))
+	cl := NewWithClient(server.URL, "123", server.Client())
+
+	resp, err := cl.GetConfig().WithCommand(context.Background(), "show int", models.LibraryNetmiko, testing2.GetConnectionArgs())
+	assert.NoError(t, err)
+
+	resp, err = cl.WaitForResult(context.Background(), resp)
+	assert.NoError(t, err)
+	assert.Equal(t, models.TaskStatusFinished, resp.Data.TaskStatus)
 }
 
 func TestClient_GetConfig(t *testing.T) {
