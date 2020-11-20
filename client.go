016c59c3b0e747db7e4714ce731fc9eec69182f0
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-20 12:14:26 +0100

    Client: Add blocking wait for result function

--- client.go
+++ client.go
@@ -1,11 +1,15 @@
 package netpalmgo
 
 import (
+	"context"
 	"github.com/jpbede/netpalmgo/apis/getconfig"
 	"github.com/jpbede/netpalmgo/apis/setconfig"
 	"github.com/jpbede/netpalmgo/apis/task"
 	"github.com/jpbede/netpalmgo/internal/transport"
+	"github.com/jpbede/netpalmgo/models"
 	"net/http"
+	"sync"
+	"time"
 )
 
 // Client is the main client of netpalmgo
@@ -33,6 +37,35 @@ func NewWithClient(apiURL, apiKey string, httpClient *http.Client) *Client {
 	c.task = task.New(c.transport)
 
 	return &c
+}
+
+// WaitForResult waits blocking for the result to finish (also fail).
+func (c *Client) WaitForResult(ctx context.Context, response *models.Response) (*models.Response, error) {
+	wg := sync.WaitGroup{}
+	resp := response
+	var err error
+
+	// wait for task to finish in other goroutine
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// sleep for 2 sec if task is not finished or failed and then check status again
+		for resp.Data.TaskStatus != models.TaskStatusFinished && resp.Data.TaskStatus != models.TaskStatusFailed {
+			resp, err = c.Task().WithTaskResponse(ctx, resp.Data)
+			if err != nil { // exit when error occurs
+				return
+			}
+			time.Sleep(2 * time.Second)
+		}
+	}()
+	wg.Wait()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, err
 }
 
 // GetConfig returns the client for the Endpoint /getconfig
