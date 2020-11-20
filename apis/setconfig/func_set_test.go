371cdc1b290baa33cc16cd6ba1e34b7fbf28a439
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-20 11:04:32 +0100

    Test: Invalid json response test /setconfig

--- apis/setconfig/func_set_test.go
+++ apis/setconfig/func_set_test.go
@@ -44,3 +44,27 @@ func TestClient_Set(t *testing.T) {
 	assert.Equal(t, models.TaskStatusQueued, resp.Data.TaskStatus)
 	assert.Equal(t, "b88975a9-d460-461a-89f8-f5f9c3dc8701", resp.Data.TaskID)
 }
+
+func TestClient_SetInvalidJson(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte("no json"))
+	}))
+
+	setConfigClient := setconfig.New(transport.NewClient(server.URL, "123", server.Client()))
+
+	setConfigReq := models.SetConfigRequest{
+		Library: models.LibraryNapalm,
+		ConnectionArgs: models.ConnectionArgs{
+			DeviceType: "vyos",
+			Host:       "10.10.10.10",
+			Username:   "demo",
+			Password:   "demo",
+		},
+		Config:        []string{"set int tunnel tun0 remote-ip 123.123.123.123"},
+		QueueStrategy: models.QueueStrategyFIFO,
+	}
+
+	_, err := setConfigClient.Set(context.Background(), false, setConfigReq)
+	assert.Error(t, err)
+	assert.EqualError(t, err, "invalid character 'o' in literal null (expecting 'u')")
+}
