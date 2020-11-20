0cda91b2c3a90e99504f17f0e7453a8bd7499d79
Jan-Philipp Benecke <jpb@cleverreach.com>
2020-11-20 11:48:35 +0100

    Transport Client: Rework error check

--- internal/transport/error.go
+++ internal/transport/error.go
@@ -9,23 +9,19 @@ import (
 
 // CheckForHTTPError does some generic response checks and raises a error when it detects some
 func CheckForHTTPError(response *http.Response) error {
-	// check if authentication error
-	if response.StatusCode == http.StatusForbidden {
+	if response.StatusCode >= 300 {
+		read, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return err
+		}
+
 		var errArr map[string]string
-		read, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return err
+		if err := json.Unmarshal(read, &errArr); err == nil {
+			return errors.New(errArr["detail"])
+		} else {
+			return errors.New(string(read))
 		}
-		json.Unmarshal(read, &errArr)
-		return errors.New(errArr["detail"])
 	}
-	// invalid request
-	if response.StatusCode == http.StatusUnprocessableEntity {
-		read, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return err
-		}
-		return errors.New(string(read))
-	}
+
 	return nil
 }
